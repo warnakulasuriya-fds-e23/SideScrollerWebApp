b5122620dc48c27d651bcd9b95eb7246f3a970aa
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-06-25 15:36:26 +0530

    added in a nav Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { Label, RangeSlider } from "flowbite-react";
+import { NavigationBar } from "./components/NavigationBar";
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <p className="text-red-500">Hello</p>
       <Label htmlFor="default-range" value="Default" />
       <RangeSlider id="default-range" />
---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,34 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+export function NavigationBar() {
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand>
+        <img
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Side Scroller
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#">About</NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
