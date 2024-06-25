740155241c344ae547898081a9a6b7c71cfe13f6
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-06-25 15:06:11 +0530

    added in flowbite-react and tried it out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-
+import { Label, RangeSlider } from "flowbite-react";
 function App() {
   return (
     <div className="App">
       <p className="text-red-500">Hello</p>
+      <Label htmlFor="default-range" value="Default" />
+      <RangeSlider id="default-range" />
     </div>
   );
 }
