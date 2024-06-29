ae05000bc2e330b2518e41c8f8bb2d46c326e30e
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-06-29 13:37:19 +0530

    styled the PageNotfound page

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -1,4 +1,23 @@
-import { Card } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
+import { HiOutlineEmojiSad } from "react-icons/hi";
 export const PageNotFound = () => {
-  return <Card>Page was not found</Card>;
+  return (
+    <div>
+      <HiOutlineEmojiSad className="w-96 h-96 mx-auto text-black dark:text-white" />
+      <div className="text-7xl text-center dark:text-white">
+        Page was not found!
+      </div>
+      <div className="text-5xl text-center dark:text-white">
+        would you like to go to Home page?
+      </div>
+      <Button
+        className="mx-auto my-10 w-36"
+        size="xl"
+        gradientDuoTone="greenToBlue"
+      >
+        <Link to="/">Home</Link>
+      </Button>
+    </div>
+  );
 };
