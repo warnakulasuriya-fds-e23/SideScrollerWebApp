0efbf98eee5388fcb1466cda00c14a73061acc26
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-07-01 01:10:30 +0530

    Planned working on a Cabinet.js component

---
+++ frontend/src/components/Cabinet.js
@@ -0,0 +1,10 @@
+//File should make a vertical bar that has several buttons.
+/*buttons are for
+game Settings
+Save Game
+Restart Game
+Load Game
+Game Screenshot(maybe)
+
+each of these buttons should a flowbite Drawer and these drawer should contain further
+options and buttons inside, according to the extent that is required.*/
