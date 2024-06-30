56a1449cb74af1026c926132c3f8d006c2bca2e1
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-07-01 00:29:01 +0530

    Styled the Game Settings box
     Also made sure that Game Settings Box only loads up when the game Settings context has had its state properly loaded with the gameSettings fetched from the backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { GameSettingsBox } from "../components/GameSettingsBox";
+import { useLoadUpGameSettings } from "../hooks/useLoadUpGameSetting";
+import { useGameSettingsContext } from "../hooks/useGameSettingsContext";
+
 export const Home = () => {
+  const { gameSettings } = useGameSettingsContext();
+  const { LoadUpGameSettings } = useLoadUpGameSettings();
+
+  useEffect(() => {
+    const loadingfunction = async () => {
+      await LoadUpGameSettings();
+    };
+    loadingfunction();
+  }, []);
   return (
     <>
       <div>HOME</div>
-      <GameSettingsBox />
+      {gameSettings && <GameSettingsBox />}
     </>
   );
 };
