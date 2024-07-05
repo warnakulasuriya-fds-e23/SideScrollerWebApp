5b2fca62f16f9934cbe403fabf64093ac03d223c
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-07-05 08:23:07 +0530

    used the useRef hook, gameState object can change without component rerendering
    also created a GameSatate class file in the game files so that it can be used with the useRef

---
+++ frontend/src/GameFiles/GameState.js
@@ -0,0 +1,53 @@
+import { KeyBoardConfiguration } from "./KeyboardConfig.js";
+import { Player } from "./player.js";
+import { InputHandler } from "./input.js";
+import { Background } from "./Background.js";
+import { UIHandler } from "./UIHandler.js";
+import { EnemyHandler } from "./EnemyHandler.js";
+import { CollisionHandler } from "./CollisionHandler.js";
+import { PickUpHandler } from "./PickupHandler.js";
+
+export class GameState {
+  constructor(width, height) {
+    this.keyboardConfig = new KeyBoardConfiguration();
+    this.paused = false;
+    this.width = width;
+    this.height = height;
+    this.score = 0;
+    this.gameTime = 0;
+    this.groundMargin = 80;
+    this.speedFraction = 0;
+    this.maxSpeed = 5;
+    this.debugMode = false;
+    this.background = new Background(this);
+    this.player = new Player(this);
+    this.input = new InputHandler(this);
+    this.enemyHandler = new EnemyHandler(this);
+    this.UIHandler = new UIHandler(this);
+    this.collisionHandler = new CollisionHandler(this);
+    this.PickUpHandler = new PickUpHandler(this);
+  }
+  update(deltaTime) {
+    this.gameTime += deltaTime;
+    this.background.update();
+    this.player.update(this.input.keys, deltaTime);
+    this.enemyHandler.update(deltaTime);
+    this.UIHandler.update();
+    this.collisionHandler.update(deltaTime);
+    this.PickUpHandler.update(deltaTime);
+  }
+  draw(context) {
+    this.background.draw(context);
+    this.player.draw(context);
+    this.enemyHandler.draw(context);
+    this.UIHandler.draw(context);
+    this.collisionHandler.draw(context);
+    this.PickUpHandler.draw(context);
+  }
+  Pause() {
+    this.paused = !this.paused;
+    if (this.paused) {
+      this.UIHandler.pauseScreen();
+    }
+  }
+}
--- frontend/src/components/GameCanvas.js
+++ frontend/src/components/GameCanvas.js
@@ -1,12 +1,48 @@
-import React, { useState } from "react";
-import { RunGame } from "../GameFiles/index";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { RunGame } from "../GameFiles/index";
 import { Button } from "flowbite-react";
+import { GameState } from "../GameFiles/GameState.js";
 export const GameCanvas = () => {
+  const navaigate = useNavigate();
   const [isGameRunning, setIsGameRunnig] = useState(false);
-  const playgame = () => {
-    setIsGameRunnig(!isGameRunning);
-    RunGame();
+  const gameRef = useRef(null);
+  useEffect(() => {}, []);
+
+  const pauseGame = () => {
+    if (isGameRunning) {
+      gameRef.current.Pause();
+    }
   };
+  const runGame = () => {
+    if (!isGameRunning) {
+      const canvas = document.getElementById("gameCanvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = 1000;
+      canvas.height = 500;
+
+      gameRef.current = new GameState(canvas.width, canvas.height);
+      let lastTime = 0;
+      function animate(timeStamp) {
+        let deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        if (gameRef.current.paused == false) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          gameRef.current.update(deltaTime);
+          gameRef.current.draw(ctx);
+        }
+
+        requestAnimationFrame(animate); //this function passes the current time stamp to the animate function automatically
+      }
+      animate(0);
+      setIsGameRunnig(true);
+    }
+  };
+
+  const goHome = () => {
+    navaigate("/*");
+  };
+
   return (
     <div>
       {/* {isGameRunning ? (
@@ -14,8 +50,13 @@ export const GameCanvas = () => {
       ) : (
         <Button onClick={playgame}>Play Game</Button>
       )} */}
-      <canvas id="gameCanvas"></canvas>
-      <Button onClick={playgame}>Play Game</Button>
+      <canvas
+        className={isGameRunning ? "block" : "hidden"}
+        id="gameCanvas"
+      ></canvas>
+      <Button onClick={pauseGame}>Pause Game</Button>
+      <Button onClick={runGame}>Run Game</Button>
+      <Button onClick={goHome}>Stop Game</Button>
     </div>
   );
 };
