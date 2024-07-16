9a6b80a12223a5f42a7274581bbf3e4ae4a853df
warnakulasuriya-fds-e23 <warnakulasuriya_fds_e23@engug.ruh.ac.lk>
2024-07-16 17:01:48 +0530

    useAuthContext was for some reason saved in commits as UseAuthContext
    i renamed the file but i have no idea why

--- frontend/src/hooks/UseAuthContext.js
+++ frontend/src/hooks/UseAuthContext.js
@@ -1,6 +1,5 @@
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
-
 export const useAuthContext = () => {
   const context = useContext(AuthContext);
   if (!context)
